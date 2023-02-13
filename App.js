10c0ef6f9ccd7e15412d43027fc60c9389d6bcd1
Ian Finley <ianlukefinley@gmail.com>
2023-02-13 13:23:33 -0500

    Lesson 9 - ToDo App1

--- App.js
+++ App.js
@@ -1,41 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import React, {useState} from 'react';
+import Header from './Components/Header';
 
 export default function App() {
 
-const [people, setPeople] = useState([
-  {name: 'ian', id: '1'},
-  {name: 'matt', id: '2'},
-  {name: 'jon', id: '3'},
-  {name: 'george', id: '4'},
-  {name: 'james', id: '5'},
-  {name: 'benjamin', id: '6'},
-  {name: 'seph', id: '7'},
-]);
-
-const pressHandler = (id) => {
-  console.log(id);
-  setPeople((prevPeople) => {
-    return prevPeople.filter( person => person.id != id)
-  })
-}
-
+  const [todos, setTodos] = useState([
+    {text: 'roast potatoes', key: '1'},
+    {text: 'create an app', key: '2'},
+    {text: 'fry bacon', key: '3'},
+  ]);
 
   return (
     <View style={styles.container}>
-
-      <FlatList 
-        numColumns={2}
-        keyExtractor={(item) => item.id}
-        data={people}
-        renderItem={({item}) => (
-          <TouchableOpacity onPress={() => {pressHandler(item.id)}}>
-            <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
-
+      <Header />
+      <View style={styles.content}>
+        {/*to form */}
+        <View style={styles.list}>
+          <FlatList 
+            data = {todos}
+            renderItem = {({item}) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -44,18 +33,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: 40,
-    paddingHorizontal: 20
-    //alignItems: 'center',
-    //justifyContent: 'center',
   },
-  item: {
-    marginTop: 30,
-    padding: 30,
-    backgroundColor: "pink",
-    fontSize: 34,
-    marginHorizontal: 10,
-  }
- 
+  content: {
+    padding:40
+  },
+  list: {
+    marginTop: 20
+  },
+
 
 });
