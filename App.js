7da90de34eb7aa67125ad5b3f5673447e6e924db
Ian Finley <ianlukefinley@gmail.com>
2023-02-13 11:06:48 -0500

    Lesson 4

--- App.js
+++ App.js
@@ -1,16 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
+
+const [name, setName] = useState("Ian");
+const [enemy, setEnemy] = useState({ name: 'Mario', age: 40})
+
+const nameHandler = () => {
+  name === "Ian" ? setName("Ethan") : setName("Ian");
+}
+
+const enemyHandler = () => {
+  enemy.name === "Mario" ? setEnemy({ name: 'Luigi', age: 42}) : setEnemy({ name: 'Mario', age: 40});
+}
+
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello, World!</Text>
+      <View style={styles.buttonContainer}>
+        <Button title='Update Name' onPress={nameHandler} />
       </View>
-            <View style={styles.body}>
-        <Text>This is Ian's app!</Text>
-        <Text>Still Ian's app!</Text>
+      <View style={styles.body}>
+        <Text>This is {name}'s app!</Text>
       </View>
+      <View style={styles.enemyBody}>
+        <Text>My enemy is {enemy.name}, who is {enemy.age}!</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title='Update Enemy' onPress={enemyHandler} />
+      </View>
+      
+
     </View>
   );
 }
@@ -22,12 +43,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  header: {
-    backgroundColor: 'pink',
-    padding: 20,
+  buttonContainer: {
+    marginTop: 20
   },
   body: {
+    marginTop: 20,
     backgroundColor: 'yellow',
+    padding: 20,
+  },
+  enemyBody: {
+    marginTop: 20,
+    backgroundColor: 'red',
     padding: 20,
   },
   boldText: {
