8b9d5d1b5b9d8a87efd6a38dd5892489cd1f2549
Ian Finley <ianlukefinley@gmail.com>
2023-02-14 09:27:13 -0500

    Lesson 13 - Dismissing the Keyboard

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, Alert } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import React, {useState} from 'react';
 import Header from './Components/Header';
 import TodoItem from './Components/TodoItem';
@@ -34,20 +34,25 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Header />
-      <View style={styles.content}>
-          <TodoForm submitHandler={submitHandler} />
-        <View style={styles.list}>
-          <FlatList 
-            data = {todos}
-            renderItem = {({item}) => (
-              <TodoItem item={item} pressHandler={pressHandler}/>
-            )}
-          />
+    <TouchableWithoutFeedback onPress={() => {
+      console.log('Dismiss Keyboard');
+      Keyboard.dismiss();
+    }} >
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+            <TodoForm submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList 
+              data = {todos}
+              renderItem = {({item}) => (
+                <TodoItem item={item} pressHandler={pressHandler}/>
+              )}
+            />
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
